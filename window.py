40f479d3067334e05554ba89ba5b3e37c1c7075c
Aditya Saini <adityasaini145@gmail.com>
2025-12-27 00:06:35 -0500

    added delta

--- window.py
+++ window.py
@@ -12,7 +12,8 @@ label = tk.Label(
     root,
     text="",
     fg="white",
-    bg="#2b2b2b"
+    bg="#2b2b2b",
+    font=('Courier', 15)
 )
 label.pack(anchor='nw', padx=20, pady=10)
 
@@ -22,13 +23,13 @@ button_frame.pack(anchor='nw', padx=20)
 info_frame = tk.Frame(root, bg="#2b2b2b")
 info_frame.pack(anchor='nw', padx=20, pady=0)
 
-
 kalshi_label = tk.Label(
     info_frame, 
     text="",
     fg="white",
     bg="#2b2b2b",
-    justify="left"
+    justify="left",
+    font=('Courier', 11)
 )
 
 kalshi_logo = tk.Label(
@@ -36,15 +37,20 @@ kalshi_logo = tk.Label(
     text="",
     fg="#4DE4B2",
     bg="#2b2b2b",
-    justify="right"
+    justify="right",
+    font=('Courier', 11)
 )
 
-kalshi_label.pack(side=tk.LEFT, padx = 0, pady = 0)
+kalshi_label.pack(side=tk.LEFT, padx=0, pady=0)
 kalshi_logo.place(x=300, y=35)
-kalshi_logo.config(font=('Courier', 11))
 
 def kalshi_button_clicked():
-    template = f"Cash: ${main.values()['cash']}\nInvested: ${main.values()['invested']}\nTransferred: ${main.values()['transferred']}"
+    if main.values()['transferred'] > (main.values()['cash'] + main.values()['invested']):
+        signToUse = "-"
+    else:
+        signToUse = "+"
+    updown = main.values()['transferred'] - (main.values()['cash'] + main.values()['invested'])
+    template = f"Cash: ${main.values()['cash']}\nInvested: ${main.values()['invested']}\nTransferred: ${main.values()['transferred']}\n\nTotal Change: {signToUse}${updown:.2f}"
     kalshi_label.config(text=template)
     kalshi_logo.config(text="""██╗  ██╗
 ██║ ██╔╝
@@ -57,11 +63,21 @@ def kalshi_button_clicked():
 def coinbase_button_clicked():
     print("Coinbase was clicked")
 
-kalshi_button = tk.Button(button_frame, text="Kalshi", command=kalshi_button_clicked)
+kalshi_button = tk.Button(
+    button_frame, 
+    text="Kalshi", 
+    command=kalshi_button_clicked,
+    font=('Courier', 10)
+)
 kalshi_button.pack(side=tk.LEFT, anchor='nw')
 
-coinbase_button = tk.Button(button_frame, text="Coinbase", command=coinbase_button_clicked)
-coinbase_button.pack(side=tk.LEFT, anchor='nw', padx = 5)
+coinbase_button = tk.Button(
+    button_frame, 
+    text="Coinbase", 
+    command=coinbase_button_clicked,
+    font=('Courier', 10)
+)
+coinbase_button.pack(side=tk.LEFT, anchor='nw', padx=5)
 
 def updateWelcome():
     now = time.localtime()
@@ -73,12 +89,17 @@ def updateWelcome():
         current = "Afternoon"
     else:
         current = "Evening"
-    label.config(text="Good " + current + "!", font=("Arial", 15))
+    label.config(text="Good " + current + "!")
     root.after(1000, updateWelcome)
 
 def update_kalshi_data():
     data = main.values()
-    template = f"Cash: ${data['cash']}\nInvested: ${data['invested']}\nTransferred: ${data['transferred']}"
+    if main.values()['transferred'] > (main.values()['cash'] + main.values()['invested']):
+        signToUse = "-"
+    else:
+        signToUse = "+"
+    updown = main.values()['transferred'] - (main.values()['cash'] + main.values()['invested'])
+    template = f"Cash: ${data['cash']}\nInvested: ${data['invested']}\nTransferred: ${data['transferred']}\n\nTotal Change: {signToUse}${updown:.2f}"
     kalshi_label.config(text=template)
     root.after(5000, update_kalshi_data)
 
